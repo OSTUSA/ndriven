417ba99f83d0e08174a31306428429746bbb3b75
Brian Scaturro <scaturrob@gmail.com>
2013-04-25 09:49:37 -0400

    add Application Service for Migration at app start

--- src/Presentation.Web/Global.asax.cs
+++ src/Presentation.Web/Global.asax.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Core.Application.Services;
 
 namespace Presentation.Web
 {
@@ -23,6 +20,7 @@ namespace Presentation.Web
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+            DependencyResolver.Current.GetService<IPersistenceSetup>().Setup();
         }
 
     }
