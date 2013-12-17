7e8cfe54289fccc26bc2f4b479afd37971d0908a
Justin Menkveld <justin@menklab.com>
2013-12-17 14:46:03 -0500

    Added formatting to Angular Controllers as template.

--- src/Presentation.Web/Scripts/app/controllers/RegisterCtrl.js
+++ src/Presentation.Web/Scripts/app/controllers/RegisterCtrl.js
@@ -2,34 +2,59 @@
 
 app.controller("RegisterCtrl", ["$rootScope", "$scope", "$http", "$location", "RegisterSvc", "LoginSvc", function ($rootScope, $scope, $http, $location, registerSvc, loginSvc) {
 
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    // SCOPE VARS
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    // API Calls
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    // HELPER METHODS
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    // UI MANIPULATION
+    ////////////////////////////////////////////////////////////////////////////////////
+
     // POST: register new user
-    $scope.register = function() {
+    $scope.register = function () {
         registerSvc.register.post({
-                Email: $scope.Email,
-                Name: $scope.Name,
-                Password: $scope.Password
-            },
+            Email: $scope.Email,
+            Name: $scope.Name,
+            Password: $scope.Password
+        },
             function () { // success register
                 // login automatically
                 var loginRsp = loginSvc.login.post({
                     Email: $scope.Email,
                     Password: $scope.Password,
                     Token: btoa($scope.Email + ':' + $scope.Password)
-            }, 
-                function() { // success login
+                },
+                function () { // success login
                     $rootScope.User = loginRsp;
                     $location.path('/dashboard');
                 },
-                function(result) { // fail login
+                function (result) { // fail login
                     console.log("error: " + result);
                     $rootScope.error = result;
                 });
             },
-            function(result) { // fail register
+            function (result) { // fail register
                 console.log("error: " + result);
                 $rootScope.error = result;
             });
     };
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    // INIT
+    ////////////////////////////////////////////////////////////////////////////////////
+   
     
 
 }]);
